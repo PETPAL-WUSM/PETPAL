c51fcbe4c78951646536e1f41ec85db965464c70
noahg-neuroimage <noahg@wustl.edu>
2026-01-29 09:42:47 -0600

    fix: rename to loader

--- petpal/io/image.py
+++ petpal/io/image.py
@@ -6,14 +6,14 @@ from collections.abc import Callable
 import ants
 
 
-class ImageReader:
+class ImageLoader:
     """Class for reading image files, with extensions such as Nifti or MGZ.
 
     See also: :py:docs:`~ants.image_read`
     """
-    def __init__(self, reader: Optional[Callable[[str], ants.ANTsImage]] = None):
-        self._reader = reader or ants.image_read
+    def __init__(self, loader: Optional[Callable[[str], ants.ANTsImage]] = None):
+        self._loader = loader or ants.image_read
 
     def load(self, filename: str) -> ants.ANTsImage:
         """Public read API that delegates to the configured reader"""
-        return self._reader(filename)
+        return self._loader(filename)
