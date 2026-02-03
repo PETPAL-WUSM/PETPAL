194475c15d76041cadcefafe1c8149af56bc6d84
noahg-neuroimage <noahg@wustl.edu>
2026-02-03 08:42:45 -0600

    docs: load docs, example, and attribute

--- petpal/io/image.py
+++ petpal/io/image.py
@@ -9,13 +9,30 @@ import ants
 class ImageLoader:
     """Class for reading image files, with extensions such as Nifti or MGZ.
 
-    See also: :py:docs:`~ants.image_read`
+    See also: :py:docs:`~ants.image_read`.
+
+    Example:
+
+        .. code-block:: python
+
+            from petpal.io.image import ImageLoader
+
+            image_loader = ImageLoader()
+            my_img = image_loader.load('/path/to/img.nii.gz')
+
+    :ivar _loader: Function that loads an image file as an ants.ANTsImage object.
     """
     def __init__(self, loader: Optional[Callable[[str], ants.ANTsImage]] = None):
         self._loader = loader or ants.image_read
 
     def load(self, filename: str) -> ants.ANTsImage:
-        """Public read API that delegates to the configured reader"""
+        """Public read API that delegates to the configured reader.
+        
+        Args:
+            filename (str): Path to file that will be loaded as ANTsImage.
+    
+        Returns:
+            img (ants.ANTsImage): Image object loaded into Python."""
         return self._loader(filename)
 
     def __call__(self, filename: str) -> ants.ANTsImage:
