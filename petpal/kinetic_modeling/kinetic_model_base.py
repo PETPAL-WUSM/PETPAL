c6c2b4b6fc89c36552f204961130417cae317d76
noahg-neuroimage <noahg@wustl.edu>
2026-04-16 09:44:34 -0500

    feat: type hint run

--- petpal/kinetic_modeling/kinetic_model_base.py
+++ petpal/kinetic_modeling/kinetic_model_base.py
@@ -253,7 +253,7 @@ class ParametricModel(ModelConfig):
 
 class LoganRefParametric(LoganRefConfig):
 
-    def run_parametric_model(self, pet_arr: np.ndarray):
+    def run_parametric_model(self, pet_arr: np.ndarray) -> np.ndarray:
         img_dims = pet_arr.shape
 
         result_arr = np.zeros((img_dims[0],img_dims[1], img_dims[2], len(self.fitted_pars)), float)
@@ -262,12 +262,11 @@ class LoganRefParametric(LoganRefConfig):
             for j in range(0, img_dims[1], 1):
                 for k in range(0, img_dims[2], 1):
                     voxel_tac = TimeActivityCurve(times=self.reference_tac.times,
-                                                    activity=pet_arr[i,j,k,:])
+                                                  activity=pet_arr[i,j,k,:])
                     result_arr[i,j,k,:] = self.run_model(reference_tac=self.reference_tac,
-                                                    region_tac=voxel_tac)
+                                                         region_tac=voxel_tac)
 
         return result_arr
-
 
     def __call__(self, input_image_path: str, out_image_path: str, tacs_path: str, reference_region: str, t_star: float, k2_prime: float):
         input_img = ants.image_read(input_image_path)
