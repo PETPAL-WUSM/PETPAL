ca0620d094f594220d91f55d61875e5d2dd2790c
noahg-neuroimage <noahg@wustl.edu>
2026-05-12 15:06:04 -0500

    chore: remove unused init files

--- petpal/kinetic_modeling/parametric_models/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import logan_ref_parametric
-
-def main():
-    print("PETPAL - Parametric Models")
-
-
-if __name__ == "__main__":
-    main()
-    
--- petpal/kinetic_modeling/region_models/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import logan_ref
-
-def main():
-    print("PETPAL - Regional Models")
-
-
-if __name__ == "__main__":
-    main()
-    
